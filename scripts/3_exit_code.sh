91e666b5101806c54beaaf937c393a945dd3cb9c
Mauro de Carvalho <mauro.dcg@gmail.com>
2020-09-10 22:04:13 -0300

    2 - Exit codes

---
+++ scripts/3_exit_code.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+echo "Testando os exit codes..."
+
+# Esse número pode variar de 0 até 255
+# Esse exit code não é exibido no terminal, para você poder visualizar, você precisará
+# utilizar a variável de ambiente "$?" => Ex: echo $?
+exit 0
