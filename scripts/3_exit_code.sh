f6ad37d514c13411f4fb359b8a1d6698f6d61dd3
Mauro de Carvalho <mauro.dcg@gmail.com>
2020-09-10 22:07:48 -0300

    2 - Exit codes - Mais exemplos

--- scripts/3_exit_code.sh
+++ scripts/3_exit_code.sh
@@ -2,6 +2,9 @@
 
 echo "Testando os exit codes..."
 
+# É possível executar mais de um comando na mesma linha utilizando ";"
+ls -lha; echo "Done!"
+
 # Esse número pode variar de 0 até 255
 # Esse exit code não é exibido no terminal, para você poder visualizar, você precisará
 # utilizar a variável de ambiente "$?" => Ex: echo $?
