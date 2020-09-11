92b4eb12628f7fd51b4ad07f660b5edbd0d3bc29
Mauro de Carvalho <mauro.dcg@gmail.com>
2020-09-10 21:54:10 -0300

    1 - Scripts iniciais - Hello World

---
+++ scripts/1_hello_world.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+# Este é um comentário
+echo "Hello world from BASH";
---
+++ scripts/2_hello_world.sh
@@ -0,0 +1,2 @@
+# Para executar esse comando, basta dar permissão de execução e rodar normalmente.
+echo "Hello world from sh";
